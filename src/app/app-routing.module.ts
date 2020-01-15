851111d68702726c21e2a9c12d6a67d0142dda00
Archit Dutt Sharma <ArchitS@quinnox.com>
2020-01-15 18:52:02 +0530

    added timeplot and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TimePlotComponent } from './time-plot/time-plot.component';
+import { PaceLayerComponent } from './pace-layer/pace-layer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'timeplot', component: TimePlotComponent},
+  { path: 'pacelayer', component: PaceLayerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [TimePlotComponent,PaceLayerComponent]
---
+++ src/app/pace-layer/pace-layer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pace-layer',
+  templateUrl: './pace-layer.component.html',
+  styleUrls: ['./pace-layer.component.css']
+})
+export class PaceLayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
